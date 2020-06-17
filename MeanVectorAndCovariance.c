32d1bb0cee749cd37a2b1efc95ee2f3b063f5f81
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2020-06-17 23:08:04 +0530

    Mean Vector calculation in MPI parallel

--- MeanVectorAndCovariance.c
+++ MeanVectorAndCovariance.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 #define INPUT_FILE "iris.data"
 
-long getNumberOfVectorsInFile(char inputFileName[]) {
+int getNumberOfVectorsInFile(char inputFileName[]) {
   FILE *fp;
-  long size = 0;
+  int size = 0;
   fp = fopen(inputFileName, "r");
   while(!feof(fp)) {
     char tmp[100];
@@ -17,28 +18,28 @@ long getNumberOfVectorsInFile(char inputFileName[]) {
   return size;
 }
 
-void storeNumbersFromFileRead(char inputFileName[], float **numbers, long size) {
+void storeNumbersFromFileRead(char inputFileName[], float *numbers, int size) {
   FILE *fp;
   fp = fopen(inputFileName, "r");
   for(int i = 0; i < size; i++) {
     char tmp[40];
     float a, b, c, d;
     fscanf(fp, "%f,%f,%f,%f,%s\n", &a, &b, &c, &d, tmp);
-    numbers[i][0] = a;
-    numbers[i][1] = b;
-    numbers[i][2] = c;
-    numbers[i][3] = d;
+    numbers[4*i + 0] = a;
+    numbers[4*i + 1] = b;
+    numbers[4*i + 2] = c;
+    numbers[4*i + 3] = d;
   }
   fclose(fp);
 }
 
-void findSumVector(float SumVector[], float **VectorMatrix, long start, long end) {
+void findSumVector(float SumVector[], float **VectorMatrix, int start, int end) {
   if(start == end) {
     for(int i = 0; i < 4; i++) {
       SumVector[i] = VectorMatrix[start][i];
     }
   } else if(start < end) {
-    long mid = start + (end - start)/2;
+    int mid = start + (end - start)/2;
     float SumVector1[4], SumVector2[4];
     findSumVector(SumVector1, VectorMatrix, start, mid);
     findSumVector(SumVector2, VectorMatrix, mid + 1, end);
@@ -49,7 +50,7 @@ void findSumVector(float SumVector[], float **VectorMatrix, long start, long end
   }
 }
 
-void findMeanVector(float MeanVector[], float **VectorMatrix, long number_of_vectors) {
+void findMeanVector(float MeanVector[], float **VectorMatrix, int number_of_vectors) {
   float SumVector[4];
   findSumVector(SumVector, VectorMatrix, 0, number_of_vectors - 1);
 
@@ -61,15 +62,15 @@ void findMeanVector(float MeanVector[], float **VectorMatrix, long number_of_vec
 /**
  * Function calculates the deviation matrix from mean; i.e, Xij - Xj(mean)
  */
-void findDeviationMatrix(float **DeviationMatrix, float MeanVector[], float **VectorMatrix, long start, long end) {
+void findDeviationMatrix(float **DeviationMatrix, float MeanVector[], float **VectorMatrix, int start, int end) {
   if(end - start < 4) {
-    for(long i = start; i <= end; i++) {
-      for(long j = 0; j < 4; j++) {
+    for(int i = start; i <= end; i++) {
+      for(int j = 0; j < 4; j++) {
         DeviationMatrix[i][j] = VectorMatrix[i][j] - MeanVector[j];
       }
     }
   } else if(start < end) {
-    long mid = start + (end - start)/2;
+    int mid = start + (end - start)/2;
     findDeviationMatrix(DeviationMatrix, MeanVector, VectorMatrix, start, mid);
     findDeviationMatrix(DeviationMatrix, MeanVector, VectorMatrix, mid + 1, end);
   }
@@ -78,26 +79,26 @@ void findDeviationMatrix(float **DeviationMatrix, float MeanVector[], float **Ve
 /**
  * Dot Product of two sub-vectors having elements from start to end in respective vectors A & B
  */
-float findDotProduct(float A[], float B[], long start, long end) {
+float findDotProduct(float A[], float B[], int start, int end) {
   if(end - start < 4) {
     float sum = 0;
-    for(long i = start; i <= end; i++) {
+    for(int i = start; i <= end; i++) {
       sum += (A[i] * B[i]);
     }
     return sum;
   } else {
-    long mid = start + (end - start)/2;
+    int mid = start + (end - start)/2;
     float sum1 = findDotProduct(A, B, start, mid);
     float sum2 = findDotProduct(A, B, mid + 1, end);
     return sum1 + sum2;
   }
 }
 
-void findCovarianceMatrix(float **CovarianceMatrix, float MeanVector[], float **VectorMatrix, long number_of_vectors) {
+void findCovarianceMatrix(float **CovarianceMatrix, float MeanVector[], float **VectorMatrix, int number_of_vectors) {
   float **DeviationMatrix, **DeviationMatrixTranspose;
   DeviationMatrix = (float**)malloc(number_of_vectors*sizeof(float*));
   DeviationMatrixTranspose = (float**)malloc(4*sizeof(float*));
-  for(long i = 0; i < number_of_vectors; i++) {
+  for(int i = 0; i < number_of_vectors; i++) {
     DeviationMatrix[i] = (float*)malloc(4*sizeof(float));
   }
 
@@ -107,58 +108,102 @@ void findCovarianceMatrix(float **CovarianceMatrix, float MeanVector[], float **
 
   findDeviationMatrix(DeviationMatrix, MeanVector, VectorMatrix, 0, number_of_vectors - 1);
 
-  for(long i = 0; i < 4; i++) {
-    for(long j = 0; j < number_of_vectors; j++) {
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < number_of_vectors; j++) {
       DeviationMatrixTranspose[i][j] = DeviationMatrix[j][i];
       // printf("%f ", DeviationMatrixTranspose[i][j]);
     }
     // printf("\n");
   }
 
-  for(long i = 0; i < 4; i++) {
-    for(long j = 0; j < 4; j++) {
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
       CovarianceMatrix[i][j] = findDotProduct(DeviationMatrixTranspose[i], DeviationMatrixTranspose[j], 0, number_of_vectors - 1);
     }
   }
 }
 
-int main() {
-  long number_of_vectors = 0;
-  number_of_vectors = getNumberOfVectorsInFile(INPUT_FILE);
+int main(int argc, char **argv) {
+  double elapsed_time;
+  int number_of_vectors = 0, number_of_vectors_per_process = 0;
+  float **VectorMatrix, **CovarianceMatrix, *VectorMatrixLinear, VectorMatrixLinearBuff[1000000];
+  float MeanVector[4], SumVector[4];
+  int my_rank, num_process, root = 0;
 
-  float **VectorMatrix, **CovarianceMatrix;
-  float MeanVector[4];
-  VectorMatrix = (float**)malloc(number_of_vectors*sizeof(float*));
-  CovarianceMatrix = (float**)malloc(4*sizeof(float*));
+  MPI_Init(&argc, &argv); // Initialise MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // Get my rank
+  MPI_Comm_size(MPI_COMM_WORLD, &num_process); // Get the total number of processors
 
-  for(long i = 0; i < number_of_vectors; i++) {
-    VectorMatrix[i] = (float*)malloc(4*sizeof(float));
-  }
+  if(my_rank == root) {
+    number_of_vectors = getNumberOfVectorsInFile(INPUT_FILE);
 
-  for(long i = 0; i < 4; i++) {
-    CovarianceMatrix[i] = (float*)malloc(4*sizeof(float));
-  }
-
-  storeNumbersFromFileRead(INPUT_FILE, VectorMatrix, number_of_vectors);
-
-  findMeanVector(MeanVector, VectorMatrix, number_of_vectors);
-
-  printf("Mean Vector:\n\n");
-  for(int i = 0; i < 4; i++) {
-    printf("%f\t", MeanVector[i]);
-  }
-  printf("\n\n");
-
-  findCovarianceMatrix(CovarianceMatrix, MeanVector, VectorMatrix, number_of_vectors);
-
-  printf("Covariance Matrix:\n\n");
-  for(int i = 0; i < 4; i++) {
-    for(int j = 0; j < 4; j++) {
-      printf("%f\t", CovarianceMatrix[i][j]);
+    if(number_of_vectors % num_process != 0) {
+      printf("Please enter number of processes such that it is a factor of \t%d\n Sorry for inconvenience.\n", number_of_vectors);
+      exit(0);
     }
-    printf("\n");
-  }
-  printf("\n");
 
+    VectorMatrixLinear = (float*)malloc(4*number_of_vectors*sizeof(float));
+
+    CovarianceMatrix = (float**)malloc(4*sizeof(float*));
+
+    for(int i = 0; i < 4; i++) {
+      CovarianceMatrix[i] = (float*)malloc(4*sizeof(float));
+    }
+
+    storeNumbersFromFileRead(INPUT_FILE, VectorMatrixLinear, number_of_vectors);
+  }
+
+  MPI_Bcast(&number_of_vectors, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+	MPI_Scatter(VectorMatrixLinear, 4 * (number_of_vectors/num_process), MPI_FLOAT, VectorMatrixLinearBuff, 4 * (number_of_vectors/num_process), MPI_FLOAT, root, MPI_COMM_WORLD);
+
+  number_of_vectors_per_process = number_of_vectors / num_process;
+  VectorMatrix = (float**)malloc(number_of_vectors_per_process*sizeof(float*));
+  for(int i = 0; i < number_of_vectors_per_process; i++) {
+    VectorMatrix[i] = (float*)malloc(4*sizeof(float));
+    VectorMatrix[i][0] = VectorMatrixLinearBuff[4*i + 0];
+    VectorMatrix[i][1] = VectorMatrixLinearBuff[4*i + 1];
+    VectorMatrix[i][2] = VectorMatrixLinearBuff[4*i + 2];
+    VectorMatrix[i][3] = VectorMatrixLinearBuff[4*i + 3];
+  }
+
+  float SumVectorLocal[4];
+ 
+  elapsed_time = - MPI_Wtime();
+  findSumVector(SumVectorLocal, VectorMatrix, 0, number_of_vectors_per_process - 1);
+
+  for(int i = 0; i < 4; i++) {
+    MPI_Allreduce(&SumVectorLocal[i], &SumVector[i], 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+  }
+
+  if(my_rank == root) {
+    for(int i = 0; i < 4; i++) {
+      MeanVector[i] = SumVector[i] / number_of_vectors;
+    }
+    elapsed_time += MPI_Wtime();
+
+    printf("Time taken for Mean Vector calculation\t%lf\n", elapsed_time);
+    printf("Mean Vector:\n\n");
+    for(int i = 0; i < 4; i++) {
+      printf("%f\t", MeanVector[i]);
+    }
+    printf("\n\n");
+  }
+
+  if(my_rank == root) {
+    // findCovarianceMatrix(CovarianceMatrix, MeanVector, VectorMatrix, number_of_vectors_per_process);
+  }
+
+  if(my_rank == root) {
+    // printf("Covariance Matrix:\n\n");
+    // for(int i = 0; i < 4; i++) {
+    //   for(int j = 0; j < 4; j++) {
+    //     printf("%f\t", CovarianceMatrix[i][j]);
+    //   }
+    //   printf("\n");
+    // }
+    // printf("\n");
+  }
+  MPI_Finalize();
   return 0;
 }
