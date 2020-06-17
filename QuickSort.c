b7563ee34c3ca3be12b80ed2125888e8ed363353
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2020-06-18 01:03:43 +0530

    MPI programs (#1)
    * mpi implementation for random number generation and QuickSort algorithm
    
    * updated Readme.md
    
    * fixed check logic in QuickSort
    
    * Mean Vector calculation in MPI parallel
    
    * Covariance matrix calculation in parallel processing using mpi
    
    * K Mean Cluster parallel implementation using MPI
    
    * newline print

--- QuickSort.c
+++ QuickSort.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
-void swap(long *a, long *b) {
-  long tmp = *a;
+void swap(int *a, int *b) {
+  int tmp = *a;
   *a = *b;
   *b = tmp;
+}
+
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+int max(int a, int b) {
+	return a > b ? a : b;
 }
 
 /**
  * Utility to print and check slice of array.
  * Will be useful when checking each process chunck of array before and after the QuickSort call.
  */
-void printArraySlice(long numbers[], long start, long end) {
-  printf("Start: %ld\t End: %ld\n", start, end);
-  for (long i = start; i <= end; i++) {
-    printf("%ld\t", numbers[i]);
+void printArraySlice(int numbers[], int start, int end) {
+  printf("Start: %d\t End: %d\n", start, end);
+  for (int i = start; i <= end; i++) {
+    printf("%d\t", numbers[i]);
   }
   printf("\n\n");
 }
@@ -22,11 +32,11 @@ void printArraySlice(long numbers[], long start, long end) {
  * Creates the partition choosing a pivot element.
  * Returns the partition point for QuickSort.
  */
-long Partition(long numbers[], long start, long end) {
-  long pivot = numbers[end];
-  long i = start - 1;
+int Partition(int numbers[], int start, int end) {
+  int pivot = numbers[end];
+  int i = start - 1;
 
-  for(long j = start; j <= end - 1; j++) {
+  for(int j = start; j <= end - 1; j++) {
     if(numbers[j] < pivot) {
       i++;
       swap(&numbers[i], &numbers[j]);
@@ -38,9 +48,9 @@ long Partition(long numbers[], long start, long end) {
   return i + 1;
 }
 
-void QuickSort(long numbers[], long start, long end) {
+void QuickSort(int numbers[], int start, int end) {
   if(start < end) {
-    long partitionPoint = Partition(numbers, start, end);
+    int partitionPoint = Partition(numbers, start, end);
     QuickSort(numbers, start, partitionPoint - 1);
     QuickSort(numbers, partitionPoint + 1, end);
   }
@@ -50,13 +60,13 @@ void QuickSort(long numbers[], long start, long end) {
  * Reads the file and returns the number of integers present in the file.
  * This is used to declare the numbers array for QuickSort.
  */
-long getFileSize(char inputFileName[]) {
+int getFileSize(char inputFileName[]) {
   FILE *fp;
-  long size = 0;
+  int size = 0;
   fp = fopen(inputFileName, "r");
   while(!feof(fp)) {
-    long tmp;
-    fscanf(fp, "%ld\n", &tmp);
+    int tmp;
+    fscanf(fp, "%d\n", &tmp);
     size++;
   }
   fclose(fp);
@@ -67,27 +77,93 @@ long getFileSize(char inputFileName[]) {
 /**
  * Reads integers from a file and store it in the provided "numbers" array;
  */
-void storeNumbersFromFileRead(char inputFileName[], long numbers[], long size) {
+void storeNumbersFromFileRead(char inputFileName[], int numbers[], int size) {
   FILE *fp;
   fp = fopen(inputFileName, "r");
   for(int i = 0; i < size; i++) {
-    fscanf(fp, "%ld\n", &numbers[i]);
+    fscanf(fp, "%d\n", &numbers[i]);
   }
   fclose(fp);
 }
 
-int main() {
-  char inputFileName[100];
-  long size = 0;
-  printf("Enter the input file location\n");
-  scanf("%s", inputFileName);
+int main(int argc, char* argv[]) {
+  double elapsed_time;
+  int num_process, my_rank, start, end;
+  int root = 0;
+  int *numbers;
+  int buffCount = 1000000;
+  int numbersPerProcess[buffCount], recvcount = buffCount, *sendcounts, *displs, sendCountBackToRoot = 0;
+  int size = 0;
 
-  size = getFileSize(inputFileName);
-  long numbers[size];
-  storeNumbersFromFileRead(inputFileName, numbers, size);
+  MPI_Init(&argc, &argv); // Initialise MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // Get my rank
+  MPI_Comm_size(MPI_COMM_WORLD, &num_process); // Get the total number of processors
+
+  if(my_rank == root) {
+  	char inputFileName[100];// = "randomNumbers.txt";
+	  size = 0;
+	  int endPoints[num_process][2];
+    sendcounts = (int*)malloc(num_process*sizeof(int));
+    displs = (int*)malloc(num_process*sizeof(int));
+	  int cnt = 0;
+
+	  printf("Enter the input file location\n");
+	  scanf("%s", inputFileName);
+
+	  size = getFileSize(inputFileName);
+    numbers = (int*)malloc(sizeof(int)*size);
+	  storeNumbersFromFileRead(inputFileName, numbers, size);
+
+	  endPoints[0][0] = 0;
+	  endPoints[0][1] = size - 1;
+	  cnt++;
+	  while(cnt < num_process) {
+	  	start = endPoints[0][0];
+			end = endPoints[0][1]; 
+	    int partitionPoint = Partition(numbers, start, end);
+	    for(int i = 1; i < cnt; i++) {
+	    	endPoints[i - 1][0] = endPoints[i][0];
+	    	endPoints[i - 1][1] = endPoints[i][1];
+	    }
+
+	    endPoints[cnt - 1][0] = start;
+	    endPoints[cnt - 1][1] = max(partitionPoint - 1, start);
+
+	    endPoints[cnt][0] = min(partitionPoint + 1, end);
+	    endPoints[cnt][1] = end;
+
+	  	cnt++;
+	  }
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //   printf("%d\t", numbers[i]);
+    // }
+    // printf("\n");
+    
+	  for(int chunk = 0; chunk < num_process; chunk++) {
+      sendcounts[chunk] = endPoints[chunk][1] - endPoints[chunk][0] + 1;
+      displs[chunk] = endPoints[chunk][0];
+    }
+    elapsed_time = - MPI_Wtime();
+  }
+
+	MPI_Scatter(sendcounts, 1, MPI_INT, &sendCountBackToRoot, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+  MPI_Scatterv(numbers, sendcounts, displs, MPI_INT, numbersPerProcess, recvcount, MPI_INT, root, MPI_COMM_WORLD);
 
   // printArraySlice(numbers, 0, size - 1);
-  QuickSort(numbers, 0, size - 1);
-  printArraySlice(numbers, 0, size - 1);
+  QuickSort(numbersPerProcess, 0, sendCountBackToRoot - 1);
+  // printArraySlice(numbersPerProcess, 0, sendCountBackToRoot - 1);
+
+  MPI_Gatherv(numbersPerProcess, sendCountBackToRoot, MPI_INT, numbers, sendcounts, displs, MPI_INT, root, MPI_COMM_WORLD);
+
+  if(my_rank == root) {
+    elapsed_time += MPI_Wtime();
+    printf("Time taken = %f\n", elapsed_time);
+    // printArraySlice(numbers, 0, size - 1);
+  }
+
+  MPI_Finalize();
   return 0;
 }
--- randomNumberGenerator.c
+++ randomNumberGenerator.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
-int main() {
-  long N;
-  FILE *fp;
+int main(int argc, char* argv[]) {
+  int N, num_process, printCount, my_rank;
   time_t t;
+  double elapsed_time;
 
-  printf("Enter the number of random numbers to be generated \n");
-  scanf("%ld", &N);
+  MPI_Init(&argc, &argv); // Initialise MPI
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // Get my rank
+  MPI_Comm_size(MPI_COMM_WORLD, &num_process); // Get the total number of processors
+  FILE *fp;
 
-  fp = fopen("randomNumbers.txt", "w");
+
+  if(my_rank == 0) {
+    printf("Enter the number of random numbers to be generated \n");
+    scanf("%d", &N);
+    printCount = N/num_process;
+    elapsed_time = - MPI_Wtime();
+    fp = fopen("randomNumbers.txt", "w");
+    for(int i = 0; i < N%num_process; i++) {
+      fprintf(fp, "%d\n", rand());
+    }
+    fclose(fp);
+  }
+
+  MPI_Bcast(&printCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
   srand((unsigned) time(&t));
-  for (long i = 0; i < N; i++) {
+
+  fp = fopen("randomNumbers.txt", "a");
+
+  for (long i = 0; i < printCount; i++) {
     fprintf(fp, "%d\n", rand());
   }
+
   fclose(fp);
+
+  if(my_rank == 0){  
+    elapsed_time += MPI_Wtime();
+    printf("Time taken = %f\n\n", elapsed_time);
+  }
+
+  MPI_Finalize();
 
   return 0;
 }
